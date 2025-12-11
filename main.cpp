0e079e18630fe2381aa14078f4cd5027e3cab03c
spoooktune <asriprasana5@gmail.com>
2025-12-10 22:09:13 -0800

    created struct for holding data, added list library (linked list)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
-int main(){
+const string NAMES_LIST[] = {};
 
+
+struct Node{
+    string name;
+    string order;
+    Node(string n, string o){
+        name = n; 
+        order = o;
+    }
+};
+
+int main(){
+    list<Node> coffee_booth;
     return 0;
 }
