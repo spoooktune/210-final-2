6feb26c68544eb7265ff5ea7365ed91efa0b60ea
spoooktune <asriprasana5@gmail.com>
2025-12-10 22:57:16 -0800

    copied code for coffee both and adjusted for muffin booth

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // Milestone 1
 const string NAMES_LIST[20] = {"Liam", "Emma", "Noah", "Olivia", "Ethan", "Ava", "Mason", "Sophia", "Logan", "Isabella", "Lucas", "Mia", "Alexander", "Charlotte", "Henry", "Amelia", "Jack", "Evelyn", "Samuel", "Harper"};
 const string COFFEE_LIST[20] = {"Iced vanilla latte with oat milk", "Double espresso, extra hot", "Caramel macchiato with almond milk", "Mocha frappé with whipped cream", "Flat white, whole milk", "Cold brew with sweet cream", "Americano with a splash of half-and-half", "Iced chai latte, light ice", "Cappuccino with an extra shot", "Hazelnut latte with coconut milk", "Nitro cold brew, no ice", "Pumpkin spice latte, whipped cream", "Iced matcha latte with soy milk", "Honey cinnamon latte, whole milk", "Espresso macchiato, two shots", "White chocolate mocha, no whip", "Iced americano with vanilla cold foam", "Cortado, whole milk", "Brown sugar shaken espresso, oat milk", "Drip coffee, dark roast, two sugars"};
+// Milestone 3
 const string MUFFIN_LIST[20] = {"Blueberry Crumble", "Chocolate Chip", "Banana Nut", "Lemon Poppyseed", "Double Chocolate", "Apple Cinnamon", "Raspberry White Chocolate", "Pumpkin Spice", "Cranberry Orange", "Peanut Butter Swirl", "Carrot Cake", "Strawberry Streusel", "Maple Pecan", "Mocha Espresso", "Coconut Lime", "Blackberry Almond", "Vanilla Bean", "Cherry chocolate chunk", "Gingerbread", "Salted Caramel"};
 
 // Milestone 1
@@ -24,6 +25,7 @@ int main(){
     srand(time(0));
     // Milestone 1
     list<Node> coffee_booth;
+    deque<Node> muffin_booth;
     int random;
     string n;
     string o;
@@ -32,9 +34,19 @@ int main(){
         n = NAMES_LIST[random];
         random = rand() % 20;
         o = COFFEE_LIST[random];
-        Node temp(n, o);
-        coffee_booth.push_back(temp);
-        cout << "> " << n << " joined the line [" << o << "]" << endl;
+        Node tC(n, o);
+        coffee_booth.push_back(tC);
+        cout << "[Coffee] > " << n << " joined the line [" << o << "]" << endl;
+
+        // Milestone 3
+        random = rand() % 20;
+        n = NAMES_LIST[random];
+        random = rand() % 20;
+        o = MUFFIN_LIST[random];
+        Node tM(n, o);
+        muffin_booth.push_back(tM);
+        cout << "[Muffins] > " << n << " joined the line [" << o << "]" << endl;
+
     }
     cout << endl;
 
@@ -47,25 +59,56 @@ int main(){
             n = NAMES_LIST[random];
             random = rand() % 20;
             o = COFFEE_LIST[random];
-            Node temp(n, o);
-            coffee_booth.push_back(temp);
-            cout << "> " << n << " joined the line [" << o << "]" << endl;
+            Node tC(n, o);
+            coffee_booth.push_back(tC);
+            cout << "[Coffee] > " << n << " joined the line [" << o << "]" << endl;
         }
         if (!coffee_booth.empty()){
             n = coffee_booth.front().name;
             o = coffee_booth.front().order;
             coffee_booth.pop_front();
-            cout << "> " << n << " has been served" << endl;
+            cout << "[Coffee] > " << n << " has been served" << endl;
         }
         else{
-            cout << "> No new events" << endl;
+            cout << "[Coffee] > No new events" << endl;
         }
-        cout << "> Current Line" << endl;
+
+        cout << "[Coffee] > Current Line" << endl;
         if (coffee_booth.empty()){
             cout << " > Line is empty" << endl;
         }
         else{
             for (Node p : coffee_booth){
+                cout << " > " << p.name << " [" << p.order << "]" << endl;
+            }
+        }
+
+        // Milestone 3
+        random = rand() % 100 + 1;
+        if (random <= 50){
+            random = rand() % 20;
+            n = NAMES_LIST[random];
+            random = rand() % 20;
+            o = MUFFIN_LIST[random];
+            Node tM(n, o);
+            muffin_booth.push_back(tM);
+            cout << "[Muffins] > " << n << " joined the line [" << o << "]" << endl;
+        }
+        if (!muffin_booth.empty()){
+            n = muffin_booth.front().name;
+            o = muffin_booth.front().order;
+            muffin_booth.pop_front();
+            cout << "[Muffins] > " << n << " has been served" << endl;
+        }
+        else{
+            cout << "[Muffins] > No new events" << endl;
+        }
+        cout << "[Muffins] > Current Line" << endl;
+        if (muffin_booth.empty()){
+            cout << " > Line is empty" << endl;
+        }
+        else{
+            for (Node p : muffin_booth){
                 cout << " > " << p.name << " [" << p.order << "]" << endl;
             }
         }
